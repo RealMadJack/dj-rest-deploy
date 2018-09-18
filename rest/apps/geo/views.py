9776ceb1b0367127ca41b55f4f4efa66216bdcd0
Dmitry G <gupalo1108@gmail.com>
2018-09-18 11:14:09 +0300

    Basic caching with sessions

--- rest/apps/geo/views.py
+++ rest/apps/geo/views.py
@@ -5,12 +5,20 @@ from django.shortcuts import render
 
 
 def geo_home(request):
-    response = requests.get(f'http://api.ipstack.com/check?access_key={settings.IP_STACK}')
-    geodata = response.json()
+    # del request.session['geodata']
+    is_cached = ('geodata' in request.session)
+
+    if not is_cached:
+        response = requests.get(f'http://api.ipstack.com/check?access_key={settings.IP_STACK}')
+        request.session['geodata'] = response.json()
+
+    geodata = request.session['geodata']
+
     return render(request, 'geo_home.html', {
         'ip': geodata['ip'],
         'country': geodata['country_name'],
         'latitude': geodata['latitude'],
         'longitude': geodata['longitude'],
         'google_api_key': settings.GOOGLE_MAPS_API_KEY,
+        'is_cached': is_cached,
     })
